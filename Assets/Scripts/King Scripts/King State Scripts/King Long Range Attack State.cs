9bcdb2fd40a1633d6003a6f508727afabeda0d74
BailyGameDev <mcbaily312@gmail.com>
2024-05-11 14:46:29 -0400

    King State Machine

---
+++ Assets/Scripts/King Scripts/King State Scripts/King Long Range Attack State.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingLongRangeAttackState : KingBaseState
+{
+
+
+    public override KingBaseState RunCurrentState()
+    {
+
+        return this;
+    }
+}
---
+++ Assets/Scripts/King Scripts/King State Scripts/King Main Attack State.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingMainAttackState : KingBaseState
+{
+
+
+    public override KingBaseState RunCurrentState()
+    {
+
+        return this;
+    }
+}
